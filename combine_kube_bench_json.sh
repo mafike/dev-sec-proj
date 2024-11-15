d398bb7d99467be1df9995f6c3d5c2f7299b8314
mafike <mafgenes1@icloud.com>
2024-11-15 18:42:10 -0500

    fiex

--- combine_kube_bench_json.sh
+++ combine_kube_bench_json.sh
@@ -8,26 +8,25 @@ fi
 
 # Combine JSON files into one
 echo '{ "Benchmarks": [' > combined-bench.json
-echo "Processing kube-bench-report.json..."
-cat kube-bench-report.json | jq '.Controls' > debug-kube-bench.json
-cat debug-kube-bench.json | sed 's/^\[/{ "type": "master", "controls": [/' | sed 's/\]$/] }/' >> combined-bench.json
+
+# Add master JSON
+cat kube-bench-report.json | jq '{ "type": "master", "controls": .Controls }' >> combined-bench.json
 echo ',' >> combined-bench.json
 
-echo "Processing etcd-bench-report.json..."
-cat etcd-bench-report.json | jq '.Controls' > debug-etcd-bench.json
-cat debug-etcd-bench.json | sed 's/^\[/{ "type": "etcd", "controls": [/' | sed 's/\]$/] }/' >> combined-bench.json
+# Add etcd JSON
+cat etcd-bench-report.json | jq '{ "type": "etcd", "controls": .Controls }' >> combined-bench.json
 echo ',' >> combined-bench.json
 
-echo "Processing kubelet-bench-report.json..."
-cat kubelet-bench-report.json | jq '.Controls' > debug-kubelet-bench.json
-cat debug-kubelet-bench.json | sed 's/^\[/{ "type": "kubelet", "controls": [/' | sed 's/\]$/] }/' >> combined-bench.json
+# Add kubelet JSON
+cat kubelet-bench-report.json | jq '{ "type": "kubelet", "controls": .Controls }' >> combined-bench.json
 
+# Close the JSON array
 echo '] }' >> combined-bench.json
 
 # Validate the combined JSON
 jq . combined-bench.json > /dev/null 2>&1
 if [[ $? -ne 0 ]]; then
-    echo "Failed to generate a valid combined JSON file. Debug files have been created."
+    echo "Failed to generate a valid combined JSON file. Check combined-bench.json for errors."
     exit 1
 fi
 
