f2ac5399e32769ab913ea06370b71929d1b0b604
mafike <mafgenes1@icloud.com>
2024-12-20 16:46:01 -0500

    fixed trivy

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-# Extract the Docker image name from the Dockerfile
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo "Docker Image Name: $dockerImageName"
 
-# Pre-create the Trivy cache directory
-CACHE_DIR="$WORKSPACE/trivy"
-mkdir -p "$CACHE_DIR"
+# Set the cache directory
+TRIVY_CACHE_DIR="$WORKSPACE/trivy-cache"
+mkdir -p "$TRIVY_CACHE_DIR"
+chown -R $(id -u jenkins):$(id -g jenkins) "$TRIVY_CACHE_DIR"
 
-# Ensure the Trivy cache directory is owned by Jenkins
-chown -R jenkins:jenkins "$CACHE_DIR"
+# Run Trivy scans
+docker run --rm \
+  -v "$WORKSPACE:/root/.cache/" \
+  -e "TRIVY_CACHE_DIR=/root/.cache/trivy-cache" \
+  aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light "$dockerImageName"
 
-# Run Trivy scans as the Jenkins user
-echo "Running Trivy scan for HIGH severity vulnerabilities..."
-docker run --rm --user $(id -u):$(id -g) -v "$CACHE_DIR:/root/.cache/" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light "$dockerImageName"
-
-echo "Running Trivy scan for CRITICAL severity vulnerabilities..."
-docker run --rm --user $(id -u):$(id -g) -v "$CACHE_DIR:/root/.cache/" aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light "$dockerImageName"
+docker run --rm \
+  -v "$WORKSPACE:/root/.cache/" \
+  -e "TRIVY_CACHE_DIR=/root/.cache/trivy-cache" \
+  aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light "$dockerImageName"
 
 # Trivy scan result processing
 exit_code=$?
@@ -29,7 +30,3 @@ if [[ "${exit_code}" == 1 ]]; then
 else
     echo "Image scanning passed. No CRITICAL vulnerabilities found."
 fi
-
-# Cleanup the cache directory
-echo "Cleaning up Trivy cache directory..."
-rm -rf "$CACHE_DIR"
