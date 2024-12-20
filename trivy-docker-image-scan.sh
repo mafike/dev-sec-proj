89b23fb664ffd257b134db7d69fcbb55d53aa674
mafike <mafgenes1@icloud.com>
2024-12-20 17:35:25 -0500

    run

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -1,23 +1,12 @@
 #!/bin/bash
 
+# Fetch the Docker image name from the Dockerfile
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo "Docker Image Name: $dockerImageName"
 
-# Set the cache directory
-TRIVY_CACHE_DIR="$WORKSPACE/trivy-cache"
-mkdir -p "$TRIVY_CACHE_DIR"
-chown -R $(id -u jenkins):$(id -g jenkins) "$TRIVY_CACHE_DIR"
-
 # Run Trivy scans
-docker run --rm \
-  -v "$WORKSPACE:/root/.cache/" \
-  -e "TRIVY_CACHE_DIR=/root/.cache/trivy-cache" \
-  aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light "$dockerImageName"
-
-docker run --rm \
-  -v "$WORKSPACE:/root/.cache/" \
-  -e "TRIVY_CACHE_DIR=/root/.cache/trivy-cache" \
-  aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light "$dockerImageName"
+docker run --rm -v "$WORKSPACE:/root/.cache/" aquasec/trivy:0.17.2 -q image --exit-code 0 --severity HIGH --light "$dockerImageName"
+docker run --rm -v "$WORKSPACE:/root/.cache/" aquasec/trivy:0.17.2 -q image --exit-code 1 --severity CRITICAL --light "$dockerImageName"
 
 # Trivy scan result processing
 exit_code=$?
@@ -29,4 +18,13 @@ if [[ "${exit_code}" == 1 ]]; then
     exit 1
 else
     echo "Image scanning passed. No CRITICAL vulnerabilities found."
+fi
+
+# Ensure all files and subdirectories in the trivy directory are owned by jenkins
+if [[ -d "trivy" ]]; then
+    echo "Fixing permissions for the trivy directory..."
+    sudo chown -R jenkins:jenkins trivy
+    echo "Permissions fixed."
+else
+    echo "Trivy directory not found. Skipping permission fix."
 fi
